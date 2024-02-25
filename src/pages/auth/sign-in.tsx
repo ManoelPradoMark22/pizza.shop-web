1a9230d27edf4b42365f1ab6857cbd3cbac39814
Manoel Prado <manoel.prado.neto@accenture.com>
2024-02-25 20:13:20 -0300

    resize form in small screens

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -8,8 +8,8 @@ export function SignIn() {
   return (
     <>
       <Helmet title="Login" />
-      <div className="p-8">
-        <div className="flex w-[350px] flex-col justify-center gap-6">
+      <div className="flex w-[350px] min-w-[100%] items-center justify-center p-8">
+        <div className="flex w-[100%] max-w-[400px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
               Accesar painel
