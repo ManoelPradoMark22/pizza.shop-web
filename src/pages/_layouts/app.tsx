bfee6caae501718c361fc9427a13d7d1e18062e8
Manoel Prado <manoelprado.aecjr@gmail.com>
2024-06-15 12:11:26 -0300

    automatically logout if api call is UNAUTHORIZED

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,8 +1,37 @@
-import { Outlet } from 'react-router-dom'
+import { isAxiosError } from 'axios'
+import { useEffect } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
 
 import { Header } from '@/components/header'
+import { api } from '@/lib/axios'
+import { queryClient } from '@/lib/react-query'
 
 export function AppLayout() {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const interceptorId = api.interceptors.response.use(
+      (response) => response, // a primeira funcao é disparada quando houver sucesso na requisicao
+      (error) => {
+        // a segunda funcao é disparada quando houver erro na requisicao
+        if (isAxiosError(error)) {
+          const status = error.response?.status
+
+          const code = error.response?.data?.code
+
+          if (status === 401 && code === 'UNAUTHORIZED') {
+            navigate('/sign-in', { replace: true })
+            queryClient.clear()
+          }
+        }
+      },
+    )
+
+    return () => {
+      api.interceptors.response.eject(interceptorId)
+    } // qnd o componente for desmontado, essa é a funcao de limpeza do listener do useEffect
+  }, [navigate])
+
   return (
     <div className="flex min-h-screen flex-col antialiased">
       <Header />
