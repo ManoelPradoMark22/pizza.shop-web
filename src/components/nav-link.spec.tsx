89e37ccc0b34eb50c0a2cde58a2739fd0aa05fb4
Manoel Prado <manoelprado.aecjr@gmail.com>
2024-06-16 15:36:56 -0300

    navLink test coverage

---
+++ src/components/nav-link.spec.tsx
@@ -0,0 +1,26 @@
+import { render } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
+
+import { NavLink } from './nav-link'
+
+describe('NavLink', () => {
+  it('should highlight the properly nav link based in the current active page link ', () => {
+    const wrapper = render(
+      <>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+      </>,
+      {
+        wrapper: ({ children }) => {
+          return (
+            <MemoryRouter initialEntries={['/about']}>{children}</MemoryRouter>
+          )
+        },
+      },
+    )
+    // wrapper.debug()
+
+    expect(wrapper.getByText('Home').dataset.current).toEqual('false')
+    expect(wrapper.getByText('About').dataset.current).toEqual('true')
+  })
+})
