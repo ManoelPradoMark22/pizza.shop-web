0a71f6a78c581a5aceedbba6291828d95980e5f2
Manoel Prado <manoelprado.aecjr@gmail.com>
2024-06-22 16:33:49 -0300

    sign-in.e2e-spec.ts

---
+++ test/sign-in.e2e-spec.ts
@@ -0,0 +1,41 @@
+import { expect, test } from '@playwright/test'
+
+test('sign in successfully', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' }) // aguarde tds as requisicoes do JS finalizarem
+
+  await page.getByLabel('Seu e-mail').fill('manoelprado.aecjr@gmail.com')
+
+  await page.getByRole('button', { name: 'Acessar painel' }).click()
+
+  const toast = page.getByText(
+    'Enviamos um link de autenticação para o seu e-mail.',
+  )
+
+  expect(toast).toBeVisible()
+
+  // await page.waitForTimeout(2000)
+  // apenas para que fique visivel o screenshot do ultimo step dos testes (na UI do playwright)
+  // apenas para debugar, comente sempre essa linha!
+})
+
+test('sign in with wrong credentials', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' })
+
+  await page.getByLabel('Seu e-mail').fill('wrong@gmail.com')
+
+  await page.getByRole('button', { name: 'Acessar painel' }).click()
+
+  const toast = page.getByText('Credenciais inválidas')
+
+  expect(toast).toBeVisible()
+
+  // await page.waitForTimeout(2000)
+})
+
+test('navigate to new restaurant page', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' })
+
+  await page.getByRole('link', { name: 'Novo estabelecimento' }).click()
+
+  expect(page.url()).toContain('sign-up')
+})
